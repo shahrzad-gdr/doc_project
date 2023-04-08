6e19b809d7c837111361a76fca371de37dec1ae9
Shahrzad Gdr <goudarzi.shahrzad825@gmail.com>
2023-04-08 13:41:38 +0330

    add Academy model

--- doc_app/models.py
+++ doc_app/models.py
@@ -1,2 +1,25 @@
+import datetime
+import os
+import uuid
+
+from django.contrib.auth.models import User
 from django.db import models
+
+
+
+
+class Academy(models.Model):
+    class Meta:
+        verbose_name = 'Profile'
+        verbose_name_plural = 'Profiles'
+
+
+    name         = models.CharField(max_length=20, verbose_name='academy')
+    manager_name = models.CharField(max_length=50, verbose_name='name')
+    logo         = models.ImageField(upload_to='media/logo/', verbose_name='logo')
+
+
+    def __str__(self):
+        return self.name
+
 
