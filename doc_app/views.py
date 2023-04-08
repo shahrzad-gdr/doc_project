56a58aae4457599be8313ca58412a03b072e5127
Shahrzad Gdr <goudarzi.shahrzad825@gmail.com>
2023-04-08 19:47:25 +0330

    add all_students

--- doc_app/views.py
+++ doc_app/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from io import BytesIO
 
-# Create your views here.
+from PIL import Image
+from django.shortcuts import render, redirect
+import qrcode
+import qrcode.image.svg
+from docxtpl import DocxTemplate, InlineImage
+
+from doc_app.models import Result, Course
+
+
+"""
+=========================================================================================
+    show all students
+=========================================================================================
+"""
+def all_students(request):
+
+    queryset = Result.objects.all()
+
+    context = {
+        'list' : queryset,
+    }
+    return render(request, 'doc_app/all_students.html', context)
+
