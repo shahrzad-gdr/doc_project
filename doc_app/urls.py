6f8aa51841f48bfe95afec76fc029c8e73823472
Shahrzad Gdr <goudarzi.shahrzad825@gmail.com>
2023-04-08 19:50:17 +0330

    add  views' paths

--- doc_app/urls.py
+++ doc_app/urls.py
@@ -1,5 +1,12 @@
 
 from django.urls import path
 
+from doc_app import views
+
 urlpatterns = [
+    path('', views.all_students, name='all_students'),
+    path('graduate_students/', views.graduate_students, name='graduate_students'),
+    path('studying/', views.studying, name='studying'),
+    path('courses/', views.all_courses, name='courses'),
+    path('crate_certification/<int:id>/', views.crate_certification, name='crate_certification'),
 ]
